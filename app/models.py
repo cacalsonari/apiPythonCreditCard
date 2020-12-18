204f98d755a1e3eb7123daf2c81553bd66688ad4
cacalsonari <ca.calsonari@gmail.com>
2020-12-17 23:14:29 -0300

    create the model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class CreditCardModel(models.Model):
+    
+    exp_date = models.CharField(max_length=10,blank=False)
+    holder =  models.CharField(max_length=70, blank=False)
+    number = models.CharField(max_length=16, blank=False)
+    cvv = models.IntegerField(blank=False)
+    brand = models.CharField(max_length=70, blank=False)
