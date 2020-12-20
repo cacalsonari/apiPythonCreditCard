a86f1b6fb3c4dbd860eae380c2357a0fe2b78bf2
cacalsonari <ca.calsonari@gmail.com>
2020-12-19 21:32:20 -0300

    change model

--- app/models.py
+++ app/models.py
@@ -8,4 +8,4 @@ class CreditCardModel(models.Model):
     holder =  models.CharField(max_length=70, blank=False)
     number = models.CharField(max_length=16, blank=False)
     cvv = models.IntegerField(blank=False)
-    brand = models.CharField(max_length=70, blank=False)
+    brand = models.CharField(max_length=70, blank=True)
