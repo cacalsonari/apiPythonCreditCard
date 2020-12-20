3b2d1b9bbcbed9d1739d8aecbd7fa1529c09a005
cacalsonari <ca.calsonari@gmail.com>
2020-12-19 21:37:21 -0300

    set brand by number and encrypt number

--- app/serializers.py
+++ app/serializers.py
@@ -1,8 +1,13 @@
-from rest_framework import serializers 
 import re
 import calendar
+import Crypto
+
+from Crypto import Random
+from Crypto.PublicKey import RSA
+
 from datetime import datetime
 from creditcard import CreditCard
+from rest_framework import serializers
 
 from .models import CreditCardModel
 
@@ -20,7 +25,12 @@ class CreditCardSerializer(serializers.ModelSerializer):
         
         #get last day from date and verify if date is in the past
         lastDayMonth = calendar.monthrange(date.year, date.month)[1]
-        newDate = str(date.year) + '-' + str(date.month) + '-' + str(lastDayMonth)
+        if date.month < 10:
+            month = '0' + str(date.month)
+        else: 
+            month = str(date.month)
+
+        newDate = str(date.year) + '-' + month + '-' + str(lastDayMonth)
         if datetime.today().strftime('%Y-%m-%d') > newDate :
             raise serializers.ValidationError("exp_date is a past date")
             
@@ -35,11 +45,23 @@ class CreditCardSerializer(serializers.ModelSerializer):
 
     def validate_number(self, value):
         cc = CreditCard(value)
-        #verify if creditcard is valid
+        #verify if credit card is valid
         if cc.is_valid() is False:
             raise serializers.ValidationError("number is not valid")
-                            
-        return value
+
+        #get brand from number and set in data
+        try:
+            self.initial_data['brand'] = cc.get_brand()
+        except ValueError:
+            raise serializers.ValidationError("brand not found for this number")
+        
+        #encrypt credit card number
+        randomGenerator = Random.new().read
+        key = RSA.generate(1024, randomGenerator) #generate public and privite key
+        publickey = key.publickey() # public key export for exchange
+        encryptedValue = publickey.encrypt(value.encode('utf-8'), 32)
+
+        return encryptedValue
 
     def validate_cvv(self, value):
         #verify if cvv has less than 3 numbers and more than 4 number
@@ -49,4 +71,4 @@ class CreditCardSerializer(serializers.ModelSerializer):
         if len(cvv) > 4:
             raise serializers.ValidationError("cvv has more than 4 numbers")
                     
-        return value
+        return value  
